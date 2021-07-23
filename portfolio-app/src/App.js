0091e7a01d2818514c8803650a6b5fc1b76e748a
Potears <ryan.potere@gmail.com>
2021-07-22 23:29:52 -0400

    revert

--- portfolio-app/src/App.js
+++ portfolio-app/src/App.js
@@ -1,36 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import PortfolioContainer from "./components/PortfolioContainer";
 
-import Home from './pages/Home'
-import Header from './components/Header'
-import About from './pages/About';
-import Navtabs from './components/Navtabs';
-import Projects from './pages/Projects';
-import ContactMe from './pages/ContactMe';
-
-function App() {
-    return (
-        <Router>
-            <div>
-                <Header />
-                <Navtabs />
-                <div className='mainDiv'>
-                    <Route exact path='/'>
-                        <About />
-                    </Route>
-                    <Route exact path="/home">
-                        <Home />
-                    </Route>
-                    <Route exact path="/projects">
-                        <Projects />
-                    </Route>
-                    <Route exact path="/contactMe">
-                        <ContactMe />
-                    </Route>
-                </div>
-            </div>
-        </Router>
-    )
-}
+const App = () => <PortfolioContainer />;
 
 export default App;
--- portfolio-app/src/components/Navtabs.js
+++ portfolio-app/src/components/Navtabs.js
@@ -5,25 +5,25 @@ function Navtabs({ currentPage, handlePageChange }) {
         <div>
         <ul className='nav'>
             <li className='nav-item'>
-                <a href="/home" onClick={() => handlePageChange('Home')}
+                <a href="#home" onClick={() => handlePageChange('Home')}
                 className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}>
                     Home
                 </a>
             </li>
             <li className='nav-item'>
-                <a href="/projects" onClick={() => handlePageChange('Projects')}
+                <a href="#projects" onClick={() => handlePageChange('Projects')}
                 className={currentPage === 'Projects' ? 'nav-link active' : 'nav-link'}>
                     Projects
                 </a>
             </li>
             <li className='nav-item'>
-                <a href="/" onClick={() => handlePageChange('About')}
+                <a href="#about" onClick={() => handlePageChange('About')}
                 className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}>
                     About Me
                 </a>
             </li>
             <li className='nav-item'>
-                <a href="/contactMe" onClick={() => handlePageChange('ContactMe')}
+                <a href="#contactMe" onClick={() => handlePageChange('ContactMe')}
                 className={currentPage === 'ContactMe' ? 'nav-link active' : 'nav-link'}>
                     Contact
                 </a>
